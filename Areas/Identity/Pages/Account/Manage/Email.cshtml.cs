6a096cc8b0be7496f4359f3ddf23b7d49c746da5
Yaroslav <yarryslv@gmail.com>
2021-02-03 16:05:01 +0200

    Some fixes

--- Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -45,7 +45,7 @@ namespace SimpleWebApp.Areas.Identity.Pages.Account.Manage
         {
             [Required]
             [EmailAddress]
-            [Display(Name = "New email")]
+            [Display(Name = "Новый Email")]
             public string NewEmail { get; set; }
         }
 
